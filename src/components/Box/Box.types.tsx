dc20b0e296a4e40ccec3d4f2559cc124ae95815e
Bavo Vanderghote <bavo.vanderghote@studiohyperdrive.be>
2022-01-04 12:29:12 +0100

    chore(box): remove unnecessary type

--- src/components/Box/Box.types.tsx
+++ src/components/Box/Box.types.tsx
@@ -1,9 +1,6 @@
-import { ReactNode } from 'react';
-
 import { DefaultComponentProps } from '../../types';
 
 export interface BoxProps extends DefaultComponentProps {
 	condensed?: boolean;
 	backgroundColor?: 'gray' | 'soft-white' | 'white' | 'dark';
-	children: ReactNode;
 }
