de443d0a619e1d91bf252a2f478481b97f06329a
Bavo Vanderghote <bavo.vanderghote@studiohyperdrive.be>
2022-01-05 15:38:59 +0100

    feat(badge): add storybook entry

---
+++ src/components/Badge/Badge.stories.tsx
@@ -0,0 +1,28 @@
+import { ComponentMeta, ComponentStory } from '@storybook/react';
+import React from 'react';
+
+import { Badge } from './Badge';
+
+export default {
+	title: 'Components/Badge',
+	component: Badge,
+} as ComponentMeta<typeof Badge>;
+
+const Template: ComponentStory<typeof Badge> = (args) => <Badge {...args} />;
+
+export const Default = Template.bind({});
+Default.args = {
+	text: 'Pending approval',
+};
+
+export const Success = Template.bind({});
+Success.args = {
+	text: 'Approval granted',
+	type: 'success',
+};
+
+export const Error = Template.bind({});
+Error.args = {
+	text: 'Approval denied',
+	type: 'error',
+};
