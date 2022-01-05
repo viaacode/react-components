8c520328e552895431a1f6f7cc5b28a14e2442d8
Bavo Vanderghote <bavo.vanderghote@studiohyperdrive.be>
2022-01-05 16:05:04 +0100

    test(badge): refactor tests with react-testing-library

---
+++ src/components/Badge/Badge.test.tsx
@@ -0,0 +1,60 @@
+import { render, screen } from '@testing-library/react';
+import React from 'react';
+
+import { Badge } from './Badge';
+
+const renderBadge = ({ text = 'text', ...rest }) => {
+	return render(<Badge text={text} {...rest} />);
+};
+
+describe('<Badge />', () => {
+	it('Should be able to render', () => {
+		const text = 'badge-label';
+		const { getByText } = renderBadge({ text });
+
+		const badge = getByText(text);
+
+		expect(badge).toBeInTheDocument();
+	});
+
+	it('Should render the text correctly', () => {
+		const text = 'this is a badge';
+		const { container } = renderBadge({ text });
+
+		const badge = container.firstChild;
+
+		expect(badge?.textContent).toEqual(text);
+	});
+
+	it('Should set the correct className', () => {
+		const text = 'badge-label';
+		const className = 'c-badge-custom';
+		const variants = ['success', 'error'];
+		const { getByText } = renderBadge({ text, className, variants });
+
+		const badge = getByText(text);
+
+		expect(badge).toHaveClass('c-badge');
+		expect(badge).toHaveClass(className);
+		expect(badge).toHaveClass(`c-badge--${variants[0]}`);
+		expect(badge).toHaveClass(`c-badge--${variants[1]}`);
+	});
+
+	it('Should set the correct className for every type', () => {
+		const defaultText = 'default';
+		const successText = 'success';
+		const errorText = 'error';
+
+		renderBadge({ text: defaultText });
+		renderBadge({ text: successText, type: 'success' });
+		renderBadge({ text: errorText, type: 'error' });
+
+		const defaultBadge = screen.getByText(defaultText);
+		const successBadge = screen.getByText(successText);
+		const errorBadge = screen.getByText(errorText);
+
+		expect(defaultBadge).toHaveClass('c-badge--default');
+		expect(successBadge).toHaveClass('c-badge--success');
+		expect(errorBadge).toHaveClass('c-badge--error');
+	});
+});
