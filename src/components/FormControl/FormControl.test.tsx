ccbdc781d7e834ccbece6fcaa55071b1a03839a8
Ian Emsens <ian.emsens@studiohyperdrive.be>
2022-01-18 13:56:31 +0100

    feat: tests

--- src/components/FormControl/FormControl.test.tsx
+++ src/components/FormControl/FormControl.test.tsx
@@ -4,10 +4,35 @@ import React from 'react';
 import FormControl from './FormControl';
 
 describe('<FormControl />', () => {
-	it('Should be able to render', () => {
+	it('Should be able to render children', () => {
 		const child = '123';
 		render(<FormControl>{child}</FormControl>);
 
 		expect(screen.getByText(child)).toBeDefined();
+	});
+
+	it('Should be able to render a label', () => {
+		const label = '456';
+		render(<FormControl label={label} />);
+
+		expect(screen.getByText(label)).toBeDefined();
+	});
+
+	it('Should be able to render a label with a suffix', () => {
+		const label = '789';
+		const suffix = '(optional)';
+		render(<FormControl label={label} suffix={suffix} />);
+
+		expect(screen.getByText(new RegExp(label))).toBeDefined();
+		expect(screen.getByText(new RegExp(suffix))).toBeDefined();
+	});
+
+	it('Should be able to render errors', () => {
+		const foo = 'foo';
+		const errors = ['bar', <b key={2}>{foo}</b>];
+		render(<FormControl errors={errors} />);
+
+		expect(screen.getByText(errors[0] as string)).toBeDefined();
+		expect(screen.getByText(foo)).toBeDefined();
 	});
 });
