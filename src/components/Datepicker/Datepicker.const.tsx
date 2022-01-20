31b8b1ecaed47ebf3b3d60c823334096bc135914
Ian Emsens <ian.emsens@studiohyperdrive.be>
2022-01-20 15:27:06 +0100

    feat(arc-376): minor adjustments

--- src/components/Datepicker/Datepicker.const.tsx
+++ src/components/Datepicker/Datepicker.const.tsx
@@ -2,6 +2,8 @@ import { addYears, endOfYear } from 'date-fns/esm';
 import { nlBE } from 'date-fns/locale';
 import { ReactDatePickerProps } from 'react-datepicker';
 
+const now = new Date();
+
 const base: Partial<ReactDatePickerProps> = {
 	calendarStartDay: 1,
 	showYearDropdown: true,
@@ -12,11 +14,11 @@ const base: Partial<ReactDatePickerProps> = {
 
 export const future: Partial<ReactDatePickerProps> = {
 	...base,
-	minDate: new Date(), // Now
-	maxDate: endOfYear(addYears(new Date(), 4)), // Next 5 years
+	minDate: now,
+	maxDate: endOfYear(addYears(now, 4)), // Next 5 years
 };
 
 export const historic: Partial<ReactDatePickerProps> = {
 	...base,
-	maxDate: new Date(), // Now
+	maxDate: now,
 };
