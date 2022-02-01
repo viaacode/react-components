d509046879bd5d4e56a74bcbbb4577c70490f419
Andry Charlier <andry.charlier@studiohyperdrive.be>
2022-02-01 10:17:30 +0100

    feat(arc-377): add native select and react select components

---
+++ src/components/Select/__mocks__/select.ts
@@ -0,0 +1,20 @@
+import { SelectOption } from '..';
+
+export const selectOptionsMock: SelectOption[] = [
+	{
+		label: 'Option 1',
+		value: 'option-1',
+	},
+	{
+		label: 'Option 2',
+		value: 'option-2',
+	},
+	{
+		label: 'Option 3',
+		value: 'option-3',
+	},
+	{
+		label: 'Option 4',
+		value: 'option-4',
+	},
+];
