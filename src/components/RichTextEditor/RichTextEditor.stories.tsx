fbd55ff7d91675b090ade547c300f5e207cf00b2
Bert Verhelst <verhelstbert@gmail.com>
2022-02-24 14:36:46 +0100

    fix(arc-444): convert storybook for rich text to new syntax

--- src/components/RichTextEditor/RichTextEditor.stories.tsx
+++ src/components/RichTextEditor/RichTextEditor.stories.tsx
@@ -1,5 +1,5 @@
 import { action } from '@storybook/addon-actions';
-import { storiesOf } from '@storybook/react';
+import { ComponentMeta, ComponentStory } from '@storybook/react';
 import React, { cloneElement, ReactElement, useState } from 'react';
 
 import Select from '../Select/Select';
@@ -8,21 +8,29 @@ import { selectOptionsMock } from '../Select/__mocks__/select';
 import { RichTextEditor } from './RichTextEditor';
 import { RichTextEditorControlSchema } from './RichTextEditor.types';
 
-const withContent = (story: any) => <div className="c-content">{story()}</div>;
-
-export const RICH_TEXT_EDITOR_OPTIONS: RichTextEditorControlSchema[] = [
+const RICH_TEXT_EDITOR_OPTIONS: RichTextEditorControlSchema[] = [
 	'undo',
 	'redo',
 	'separator',
+	'headings',
 	'bold',
 	'italic',
 	'underline',
+	'strike-through',
 	'separator',
 	'list-ul',
 	'list-ol',
 	'separator',
+	'subscript',
+	'superscript',
+	'separator',
 	'link',
+	'remove-styles',
 ];
+
+const MOCK_RICH_TEXT_EDITOR_PROPS = {
+	initialHtml: '<h2>Welcome!</h2><p>This prefilled content is all <strong>editable</strong>.</p>',
+};
 
 const RichTextEditorStoryComponent = ({ children }: { children: ReactElement }) => {
 	const [state, setState] = useState(null);
@@ -36,56 +44,59 @@ const RichTextEditorStoryComponent = ({ children }: { children: ReactElement }) 
 	});
 };
 
-storiesOf('components/RichTextEditor', module)
-	.addParameters({ jest: ['RichTextEditor'] })
-	.addDecorator(withContent)
-	.add('RichTextEditor', () => (
-		<RichTextEditorStoryComponent>
-			<RichTextEditor
-				initialHtml={
-					'<h2>Welcome!</h2><p>This prefilled content is all <strong>editable</strong>.</p>'
-				}
-			/>
-		</RichTextEditorStoryComponent>
-	))
-	.add('RichTextEditor disabled', () => (
-		<RichTextEditorStoryComponent>
-			<RichTextEditor
-				initialHtml={
-					'<h2>Welcome!</h2><p>This prefilled content is all <strong>editable</strong>.</p>'
-				}
-				disabled
-			/>
-		</RichTextEditorStoryComponent>
-	))
-	.add('RichTextEditor with limited buttons', () => (
-		<RichTextEditorStoryComponent>
-			<RichTextEditor controls={RICH_TEXT_EDITOR_OPTIONS} />
-		</RichTextEditorStoryComponent>
-	))
-	.add('RichTextEditor with table button', () => (
-		<RichTextEditorStoryComponent>
-			<RichTextEditor controls={[...RICH_TEXT_EDITOR_OPTIONS, 'separator', 'table']} />
-		</RichTextEditorStoryComponent>
-	))
-	.add('RichTextEditor with initial table html', () => (
-		<RichTextEditorStoryComponent>
-			<RichTextEditor
-				controls={[...RICH_TEXT_EDITOR_OPTIONS, 'separator', 'table']}
-				initialHtml={
-					'<p></p><p></p><table class="c-editor-table"><tr><td colSpan="1" rowSpan="1"><u>dit is een test</u></td><td colSpan="1" rowSpan="1"><u>dit ook</u></td><td colSpan="1" rowSpan="1"><u>ook dit</u></td></tr><tr><td colSpan="1" rowSpan="1">test</td><td colSpan="1" rowSpan="1"><strong>test</strong></td><td colSpan="1" rowSpan="1">test</td></tr><tr><td colSpan="1" rowSpan="1"></td><td colSpan="1" rowSpan="1"></td><td colSpan="1" rowSpan="1"></td></tr></table><p></p>'
-				}
-			/>
-		</RichTextEditorStoryComponent>
-	))
-	.add('RichTextEditor with select above it (z-index)', () => (
-		<>
-			<div style={{ marginBottom: '5px' }}>
-				<Select options={selectOptionsMock} />
-			</div>
+export default {
+	title: 'Components/RichTextEditor',
+	component: RichTextEditor,
+} as ComponentMeta<typeof RichTextEditor>;
 
-			<RichTextEditorStoryComponent>
-				<RichTextEditor controls={[...RICH_TEXT_EDITOR_OPTIONS, 'separator', 'table']} />
-			</RichTextEditorStoryComponent>
-		</>
-	));
+const Template: ComponentStory<typeof RichTextEditor> = (args) => (
+	<RichTextEditorStoryComponent>
+		<RichTextEditor {...args} />
+	</RichTextEditorStoryComponent>
+);
+
+const TemplateWithSelect: ComponentStory<typeof RichTextEditor> = (args) => (
+	<>
+		<div style={{ marginBottom: '5px' }}>
+			<Select options={selectOptionsMock} />
+		</div>
+
+		<RichTextEditorStoryComponent>
+			<RichTextEditor {...args} />
+		</RichTextEditorStoryComponent>
+	</>
+);
+
+export const Default = Template.bind({});
+Default.args = MOCK_RICH_TEXT_EDITOR_PROPS;
+
+export const Disabled = Template.bind({});
+Disabled.args = {
+	...MOCK_RICH_TEXT_EDITOR_PROPS,
+	disabled: true,
+};
+
+export const WithLimitedButtons = Template.bind({});
+WithLimitedButtons.args = {
+	...MOCK_RICH_TEXT_EDITOR_PROPS,
+	controls: RICH_TEXT_EDITOR_OPTIONS,
+};
+
+export const WithTableButton = Template.bind({});
+WithTableButton.args = {
+	...MOCK_RICH_TEXT_EDITOR_PROPS,
+	controls: [...RICH_TEXT_EDITOR_OPTIONS, 'separator', 'table'],
+};
+
+export const WithInitialTable = Template.bind({});
+WithTableButton.args = {
+	...MOCK_RICH_TEXT_EDITOR_PROPS,
+	controls: [...RICH_TEXT_EDITOR_OPTIONS, 'separator', 'table'],
+	initialHtml:
+		'<p></p><p></p><table class="c-editor-table"><tr><td colSpan="1" rowSpan="1"><u>dit is een test</u></td><td colSpan="1" rowSpan="1"><u>dit ook</u></td><td colSpan="1" rowSpan="1"><u>ook dit</u></td></tr><tr><td colSpan="1" rowSpan="1">test</td><td colSpan="1" rowSpan="1"><strong>test</strong></td><td colSpan="1" rowSpan="1">test</td></tr><tr><td colSpan="1" rowSpan="1"></td><td colSpan="1" rowSpan="1"></td><td colSpan="1" rowSpan="1"></td></tr></table><p></p>',
+};
+
+export const WithSelectAboveItZIndex = TemplateWithSelect.bind({});
+WithSelectAboveItZIndex.args = {
+	controls: RICH_TEXT_EDITOR_OPTIONS,
+};
