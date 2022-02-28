814aa331bc832aa5138ab1627c1e00a10a38a49d
Bavo Vanderghote <bavo.vanderghote@studiohyperdrive.be>
2022-02-28 09:49:48 +0100

    feat(arc-395): remove button styling

--- src/components/Tabs/Tab/Tab.tsx
+++ src/components/Tabs/Tab/Tab.tsx
@@ -3,6 +3,7 @@ import React, { FC } from 'react';
 
 import { bemCls, getVariantClasses } from '../../../utils';
 
+import './Tab.scss';
 import { TabProps } from './Tab.types';
 
 const Tab: FC<TabProps> = ({
