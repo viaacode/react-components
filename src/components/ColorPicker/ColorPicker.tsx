b5e5aa4ef5f20cdb85aed9d65c465bc06dd87211
Bert Verhelst <verhelstbert@gmail.com>
2022-03-18 14:51:16 +0100

    fix(arc-445): fix color picker classes, export component

--- src/components/ColorPicker/ColorPicker.tsx
+++ src/components/ColorPicker/ColorPicker.tsx
@@ -3,6 +3,8 @@ import React, { FC, useState } from 'react';
 import { HexColorPicker } from 'react-colorful';
 
 import './ColorPicker.scss';
+import { bemCls, getVariantClasses } from '../../utils';
+
 import { ColorPickerProps } from './ColorPicker.types';
 
 const ColorPicker: FC<ColorPickerProps> = ({
@@ -11,40 +13,40 @@ const ColorPicker: FC<ColorPickerProps> = ({
 	disabled = false,
 	className,
 	rootClassName: root = 'c-color-picker',
+	variants,
 	style,
 }) => {
 	const [showColorPicker, setShowColorPicker] = useState<boolean>(false);
 
+	const bem = bemCls.bind(root);
+	const rootCls = clsx(className, root, getVariantClasses(root, variants), {
+		[bem('', 'disabled')]: disabled,
+	});
+
 	return (
-		<div
-			className={clsx(root, className, { 'c-color-picker__disabled': disabled })}
-			style={style}
-		>
-			<div className="c-color-picker__controls">
+		<div className={rootCls} style={style}>
+			<div className={bem('', 'controls')}>
 				<button
-					className="c-color-picker__trigger"
+					className={bem('', 'trigger')}
 					style={{ backgroundColor: color }}
 					onClick={() => setShowColorPicker((show) => !show)}
 					{...(disabled ? { disabled } : {})}
 				/>
 				<input
-					className="c-color-picker__input"
+					className={bem('', 'input')}
 					type="text"
 					value={color}
 					onChange={(evt) => onChange(evt.target.value)}
 					{...(disabled ? { disabled } : {})}
 				/>
 				{showColorPicker && (
-					<div className="c-color-picker__picker">
+					<div className={bem('', 'picker')}>
 						<HexColorPicker color={color} onChange={onChange} />
 					</div>
 				)}
 			</div>
 			{showColorPicker && (
-				<button
-					className="c-color-picker__underlay"
-					onClick={() => setShowColorPicker(false)}
-				/>
+				<button className={bem('', 'underlay')} onClick={() => setShowColorPicker(false)} />
 			)}
 		</div>
 	);
