2ca3bb1f6a0bb35ff3cc816558c5d8e4beaac2fe
Bert Verhelst <verhelstbert@gmail.com>
2022-03-22 13:16:11 +0100

    fix(arc-445): fix tests using bem composition selectors

--- src/components/ColorPicker/ColorPicker.test.tsx
+++ src/components/ColorPicker/ColorPicker.test.tsx
@@ -30,7 +30,7 @@ describe('components/<ColorPicker />', () => {
 	it('Should render an color picker div and input field', () => {
 		const { container } = renderColorPicker({ color: '#00FF00' });
 
-		const trigger = container.querySelector('.c-color-picker__trigger');
+		const trigger = container.querySelector('.c-color-picker--trigger');
 		expect(trigger).toBeDefined();
 		expect(trigger).toHaveStyle({ backgroundColor: '#00FF00' });
 	});
@@ -39,15 +39,15 @@ describe('components/<ColorPicker />', () => {
 		const { container } = renderColorPicker({ disabled: true });
 
 		expect(container.firstChild).toBeDefined();
-		expect(container.firstChild).toHaveClass('c-color-picker__disabled');
-		expect(container.querySelector('.c-color-picker__trigger')).toHaveAttribute('disabled');
-		expect(container.querySelector('.c-color-picker__input')).toHaveAttribute('disabled');
+		expect(container.firstChild).toHaveClass('c-color-picker--disabled');
+		expect(container.querySelector('.c-color-picker--trigger')).toHaveAttribute('disabled');
+		expect(container.querySelector('.c-color-picker--input')).toHaveAttribute('disabled');
 	});
 
 	it('Should be able to open the color picker', () => {
 		const { container } = renderColorPicker({});
 
-		const trigger = container?.querySelector('.c-color-picker__trigger');
+		const trigger = container?.querySelector('.c-color-picker--trigger');
 		expect(trigger).toBeDefined();
 
 		fireEvent(
@@ -58,7 +58,7 @@ describe('components/<ColorPicker />', () => {
 			})
 		);
 
-		expect(container.querySelector('.c-color-picker__picker')).toBeVisible();
-		expect(container.querySelector('.c-color-picker__underlay')).toBeVisible();
+		expect(container.querySelector('.c-color-picker--picker')).toBeVisible();
+		expect(container.querySelector('.c-color-picker--underlay')).toBeVisible();
 	});
 });
