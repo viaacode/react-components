115949af72e8b3a0682104297f9d086ca260f87f
Silke Derudder <silke.derudder@studiohyperdrive.be>
2023-01-09 15:52:52 +0100

    feat(arc-1139): add alert component

---
+++ src/components/Alert/Alert.stories.tsx
@@ -0,0 +1,58 @@
+import { ComponentMeta, ComponentStory } from '@storybook/react';
+import React from 'react';
+
+import Alert from './Alert';
+
+export default {
+	title: 'Components/Alert',
+	component: Alert,
+} as ComponentMeta<typeof Alert>;
+
+const Template: ComponentStory<typeof Alert> = (args) => <Alert {...args} />;
+
+export const Default = Template.bind({});
+Default.args = {
+	title: 'Title',
+	content: (
+		<>
+			<p>
+				Etiam a congue diam. Aenean et diam id leo sollicitudin accumsan vel sit amet arcu.
+				Maecenas molestie lacus laoreet.
+			</p>
+			<p>
+				Duis molestie cursus eleifend. Suspendisse auctor libero nec elit ultrices suscipit.
+				Maecenas vehicula augue a lectus rutrum facilisis. Vivamus at risus consequat,
+				fringilla mauris ac, rhoncus nibh. Nunc varius, mi sollicitudin porta aliquam, augue
+				eros egestas nunc, quis accumsan ante purus id nisl. Donec egestas arcu quis arcu
+				finibus commodo. Vestibulum auctor condimentum maximus. Donec nec egestas sem. Ut ut
+				neque velit. Donec posuere tempor dolor vitae tincidunt. Quisque a diam quis erat
+				pulvinar tempor. Mauris id tincidunt erat. Duis lobortis vulputate mattis. Praesent
+				vitae semper nibh, ac consequat justo.
+			</p>
+		</>
+	),
+};
+
+export const WithIcon = Template.bind({});
+Default.args = {
+	title: 'Title',
+	icon: <div>X</div>,
+	content: (
+		<>
+			<p>
+				Etiam a congue diam. Aenean et diam id leo sollicitudin accumsan vel sit amet arcu.
+				Maecenas molestie lacus laoreet.
+			</p>
+			<p>
+				Duis molestie cursus eleifend. Suspendisse auctor libero nec elit ultrices suscipit.
+				Maecenas vehicula augue a lectus rutrum facilisis. Vivamus at risus consequat,
+				fringilla mauris ac, rhoncus nibh. Nunc varius, mi sollicitudin porta aliquam, augue
+				eros egestas nunc, quis accumsan ante purus id nisl. Donec egestas arcu quis arcu
+				finibus commodo. Vestibulum auctor condimentum maximus. Donec nec egestas sem. Ut ut
+				neque velit. Donec posuere tempor dolor vitae tincidunt. Quisque a diam quis erat
+				pulvinar tempor. Mauris id tincidunt erat. Duis lobortis vulputate mattis. Praesent
+				vitae semper nibh, ac consequat justo.
+			</p>
+		</>
+	),
+};
---
+++ src/components/Alert/Alert.types.ts
@@ -0,0 +1,9 @@
+import { ReactNode } from 'react';
+
+import { DefaultComponentProps } from '../../types';
+
+export interface AlertProps extends DefaultComponentProps {
+	title: string;
+	content: ReactNode;
+	icon?: ReactNode;
+}
