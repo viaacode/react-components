0c4641e0fbf1b091882635621e261342f24d5cf5
Silke Derudder <silke.derudder@studiohyperdrive.be>
2023-01-09 15:52:52 +0100

    feat(arc-1139): add alert component

---
+++ src/components/Alert/Alert.test.tsx
@@ -0,0 +1,68 @@
+import { render } from '@testing-library/react';
+import React from 'react';
+
+import Alert from './Alert';
+import { AlertProps } from './Alert.types';
+
+const mockTitle = 'Title';
+const mockContent = <p>content</p>;
+const mockIcon = <span />;
+
+const renderAlert = ({
+	title = mockTitle,
+	content = mockContent,
+	icon = mockIcon,
+	...rest
+}: AlertProps) => {
+	return render(<Alert title={title} content={content} icon={icon} {...rest} />);
+};
+
+describe('<Alert />', () => {
+	it('Should be able to render', () => {
+		const title = 'Alert-title';
+		const contentText = 'Alert-content';
+		const content = <p>{contentText}</p>;
+		const { getByText } = renderAlert({ title, content });
+
+		const AlertTitle = getByText(title);
+		const AlertContent = getByText(contentText);
+
+		expect(AlertTitle).toBeInTheDocument();
+		expect(AlertContent).toBeInTheDocument();
+	});
+
+	it('Should render a react node correctly', () => {
+		const title = 'title';
+		const mockContent = 'content';
+		const mockClass = 'class';
+		const content = <div className={mockClass}>{mockContent}</div>;
+		const { getByText } = renderAlert({ title, content });
+
+		const component = getByText(mockContent);
+
+		expect(component).toBeInTheDocument;
+		expect(component).toHaveClass(mockClass);
+	});
+
+	it('Should set the correct className', () => {
+		const title = 'Alert-title';
+		const rootClassName = 'c-Alert';
+		const className = 'c-Alert-custom';
+		const content = <div>content</div>;
+		const { container } = renderAlert({ title, content, className, rootClassName });
+
+		expect(container.firstChild).toHaveClass(rootClassName);
+		expect(container.firstChild).toHaveClass(className);
+	});
+
+	it('Should render an icon if provided', () => {
+		const title = 'Alert-title';
+		const className = 'c-Alert-custom';
+		const content = <div>content</div>;
+		const { container } = renderAlert({ title, content, className });
+
+		const iconContainer = container.querySelectorAll('.c-Alert__icon');
+
+		expect(iconContainer).toHaveLength(iconContainer.length);
+	});
+});
---
+++ src/components/Alert/Alert.tsx
@@ -0,0 +1,30 @@
+import clsx from 'clsx';
+import React, { FC } from 'react';
+
+import { bemCls, getVariantClasses } from '../../utils';
+
+import { AlertProps } from './Alert.types';
+
+const Alert: FC<AlertProps> = ({
+	title,
+	icon,
+	content,
+	className,
+	rootClassName: root = 'c-Alert',
+	variants,
+}) => {
+	const bem = bemCls.bind(root);
+	const rootCls = clsx(className, root, getVariantClasses(root, variants));
+
+	return (
+		<div className={rootCls}>
+			{icon && <div className={bem('icon')}>{icon}</div>}
+			<div className={bem('message')}>
+				<p className={bem('title')}>{title}</p>
+				<div className={bem('content')}>{content}</div>
+			</div>
+		</div>
+	);
+};
+
+export default Alert;
