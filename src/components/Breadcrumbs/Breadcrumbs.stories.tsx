55dd6e203017cca4284ea01ab774d49c02f4739e
Silke Derudder <silke.derudder@studiohyperdrive.be>
2023-03-07 15:44:03 +0100

    feat(Breadcrumbs): add component

---
+++ src/components/Breadcrumbs/Breadcrumbs.stories.tsx
@@ -0,0 +1,18 @@
+import { ComponentMeta, ComponentStory } from '@storybook/react';
+import React from 'react';
+
+import Breadcrumbs from './Breadcrumbs';
+import { MOCK_BREADCRUMBS } from './Breadcrumbs.mock';
+
+export default {
+	title: 'Components/Breadcrumbs',
+	component: Breadcrumbs,
+} as ComponentMeta<typeof Breadcrumbs>;
+
+const Template: ComponentStory<typeof Breadcrumbs> = (args) => <Breadcrumbs {...args} />;
+
+export const Default = Template.bind({});
+Default.args = {
+	icon: <span>-</span>,
+	items: MOCK_BREADCRUMBS,
+};
---
+++ src/components/Breadcrumbs/Breadcrumbs.test.tsx
@@ -0,0 +1,59 @@
+import { render } from '@testing-library/react';
+import React from 'react';
+
+import Breadcrumbs from './Breadcrumbs';
+import { MOCK_BREADCRUMBS } from './Breadcrumbs.mock';
+import { BreadcrumbsProps } from './Breadcrumbs.types';
+
+const renderBreadcrumbs = ({ ...props }: BreadcrumbsProps) => {
+	return render(<Breadcrumbs {...props} />);
+};
+
+describe('<Breadcrumbs />', () => {
+	it('Should set the correct className', () => {
+		const className = 'c-breadcrumbs-custom';
+
+		const { container } = renderBreadcrumbs({
+			className,
+			items: MOCK_BREADCRUMBS,
+			icon: <span>icon</span>,
+		});
+
+		const breadcrumbsComponent = container.querySelector('.c-breadcrumbs');
+
+		expect(breadcrumbsComponent).toHaveClass('c-breadcrumbs');
+		expect(breadcrumbsComponent).toHaveClass(className);
+	});
+
+	it('Should render a navigation tag', () => {
+		const { container } = renderBreadcrumbs({
+			items: MOCK_BREADCRUMBS,
+			icon: <span>icon</span>,
+		});
+
+		const breadcrumbsComponent = container.getElementsByTagName('nav');
+
+		expect(breadcrumbsComponent).toBeDefined();
+	});
+
+	it('Should render all links', () => {
+		const { container } = renderBreadcrumbs({
+			items: MOCK_BREADCRUMBS,
+			icon: <span>icon</span>,
+		});
+
+		const breadcrumbsComponent = container.querySelectorAll('.c-breadcrumbs__item');
+
+		expect(breadcrumbsComponent).toHaveLength(MOCK_BREADCRUMBS.length);
+	});
+
+	it('Should render an icon after each link except for the last link', () => {
+		const { container } = renderBreadcrumbs({
+			items: MOCK_BREADCRUMBS,
+			icon: <span>icon</span>,
+		});
+
+		const icons = container.querySelectorAll('.c-breadcrumbs__icon');
+		expect(icons).toHaveLength(MOCK_BREADCRUMBS.length - 1);
+	});
+});
