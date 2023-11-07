3937bdff44ffe72de3b9081b522c6d2b2a530b96
Bert Verhelst <bert.verhelst@studiohyperdrive.be>
2023-11-07 20:25:32 +0100

    fix(arc-1989): remove time picker from react-components

--- src/components/Timepicker/Timepicker.types.ts
+++
@@ -1,8 +0,0 @@
-import { ReactNode } from 'react';
-import { ReactDatePickerProps } from 'react-datepicker';
-
-import { DefaultComponentProps } from '../../types';
-
-export interface TimepickerProps extends DefaultComponentProps, ReactDatePickerProps {
-	children?: ReactNode;
-}
--- src/components/Timepicker/index.ts
+++
@@ -1,3 +0,0 @@
-export { default as Timepicker } from './Timepicker';
-export * from './Timepicker.types';
-export * from './Timepicker.const';
