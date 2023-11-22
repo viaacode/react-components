e5e7a10b1d477d46706b1078caf3252f47c4ab15
Ward Vercruyssen <ward.vercruyssen@studiohyperdrive.be>
2023-11-22 10:42:00 +0100

    fix(arc-2009): keep avatar aspect ratio 1/1

--- src/components/Avatar/Avatar.stories.tsx
+++ src/components/Avatar/Avatar.stories.tsx
@@ -10,8 +10,20 @@ export default {
 
 const Template: ComponentStory<typeof Avatar> = (args) => <Avatar {...args} />;
 
+const TemplateLarge: ComponentStory<typeof Avatar> = (args) => (
+	<div style={{ width: '100px', border: '1px solid red' }}>
+		<Avatar {...args} />
+	</div>
+);
+
 export const Default = Template.bind({});
 Default.args = {
+	children: <div style={{ background: 'hotpink', padding: '0.5em' }}>🚀</div>,
+	text: 'Studio Hyperdrive',
+};
+
+export const Large = TemplateLarge.bind({});
+Large.args = {
 	children: <div style={{ background: 'hotpink', padding: '0.5em' }}>🚀</div>,
 	text: 'Studio Hyperdrive',
 };
