b91b77d2591246a7b285f5c6ea8903dbfcb0f441
Bert Verhelst <verhelstbert@gmail.com>
2025-02-03 18:51:55 +0100

    fix(tests): import act from react in tests

--- src/hooks/use-callback-ref/use-callback-ref.test.ts
+++ src/hooks/use-callback-ref/use-callback-ref.test.ts
@@ -1,4 +1,5 @@
-import { act, renderHook } from '@testing-library/react-hooks';
+import { renderHook } from '@testing-library/react-hooks';
+import { act } from 'react';
 
 import { useCallbackRef } from '.';
 
