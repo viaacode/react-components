19faabb07a32180d46b04203bd9e3fc8557ae4d5
Femke Reunes <femke.reunes@studiohyperdrive.be>
2025-11-27 09:39:05 +0100

    Fix formater for hours, minutes and seconds to avoid milliseconds

--- src/utils/formatters/duration.ts
+++ src/utils/formatters/duration.ts
@@ -10,7 +10,7 @@ export function formatDurationHoursMinutesSeconds(numSeconds: number | null | un
 	const seconds: number = Math.abs(numSeconds || 0);
 	const hours = Math.floor(seconds / 3600);
 	const mins = Math.floor((seconds - hours * 3600) / 60);
-	const secs = seconds % 60;
+	const secs = Math.floor(seconds % 60);
 
 	return `${hours.toString().padStart(2, '0')}:${mins.toString().padStart(2, '0')}:${secs
 		.toString()
