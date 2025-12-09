616bb5c807df5212bb75c67d2d652dde3d506f9e
Bert Verhelst <verhelstbert@gmail.com>
2025-12-09 15:38:21 +0100

    fix(AVO-3804): disable rendering flowplayer during server side rendering

--- src/components/FlowPlayer/FlowPlayer.tsx
+++ src/components/FlowPlayer/FlowPlayer.tsx
@@ -1,7 +1,6 @@
 import { type FunctionComponent, lazy, type ReactNode, Suspense } from 'react';
-
+import { isServerSideRendering } from '../../utils/is-server-side-rendering';
 import { Flex } from '../Flex/Flex';
-
 import type { FlowPlayerProps } from './FlowPlayer.types';
 
 const FlowplayerInternal = lazy(() => import('./FlowPlayer.internal.js'));
@@ -9,6 +8,10 @@ const FlowplayerInternal = lazy(() => import('./FlowPlayer.internal.js'));
 export const FlowPlayer: FunctionComponent<
 	FlowPlayerProps & { renderLoader?: () => ReactNode }
 > = ({ renderLoader, ...props }) => {
+	if (isServerSideRendering()) {
+		// Can't render Flowplayer during server side rendering since it requires the browser window / document
+		return null;
+	}
 	return (
 		<Suspense
 			fallback={
