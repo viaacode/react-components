b03af901da9db6096d611d30f27a22844a726acb
andreasN234 <andreas.lauwers@numble.be>
2026-04-15 14:37:05 +0200

    remove react-hooks

--- src/hooks/use-callback-ref/use-callback-ref.test.ts
+++ src/hooks/use-callback-ref/use-callback-ref.test.ts
@@ -1,4 +1,4 @@
-import { renderHook } from '@testing-library/react-hooks';
+import { renderHook } from '@testing-library/react';
 import { act } from 'react';
 
 import { useCallbackRef } from './index';
